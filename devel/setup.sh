6686ed5b8d50c6027a7a9006e1c2a81936f5dacc
lamdv <lam.dv@live.com>
2019-03-27 22:31:25 +0100

    coffee-me: Detected group of people

--- devel/setup.sh
+++ devel/setup.sh
@@ -10,7 +10,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/sarayev/catkin_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/mnt/c/Users/Vu Lam/Documents/RoboticsLab1/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
